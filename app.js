94f62221d0cfdeb9f567567731fa52aa5d69e4c9
mesopotato <dittrich.yannick@gmail.com>
2019-12-04 19:27:34 +0100

    prod keys

--- app.js
+++ app.js
@@ -44,14 +44,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
 
-// app.use(require('express-session')({
-//   secret: 'keyboard cat',
-//   resave: true,
-//   saveUninitialized: true
-// }));
-
-
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
---
+++ config/prod.js
@@ -0,0 +1,6 @@
+//production keys 
+module.exports = {
+    GOOGLE_CLIENT_ID : process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET : process.env.GOOGLE_CLIENT_SECRET,
+    cookieKey: process.env.COOKIE_KEY
+}
