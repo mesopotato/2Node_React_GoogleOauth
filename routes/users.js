cc2b489a6c7146083c55bc8f277d8663f0de4ebe
mesopotato <dittrich.yannick@gmail.com>
2019-12-04 12:01:21 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a resource after deployment ');
 });
-
 
 
 module.exports = router;
