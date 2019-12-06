2f471e07ac7086fc1973f4a344e791e2f1d3c329
mesopotato <dittrich.yannick@gmail.com>
2019-12-06 14:04:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -47,12 +47,12 @@ app.use('/auth', authRouter);
 app.use('/billing', billingRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -61,6 +61,18 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//configuration for production static react (build)
+if (process.env.NODE_ENV === 'production') {
+  //Express will serve up production assets 
+  // like main.js or main.css 
+  app.use(express.static('client/build'));
+
+  //Express seres the index.htmal file if the route is not recognized
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // this is in bin/www ..? aparently has to be set this time 
 const PORT = process.env.PORT || 5000;
